ba43d45b868bed95e45a3a38732e6f2cfffc0110
IvanKoshel98 <GitHubPassvord142>
2022-12-05 16:00:38 +0200

    Search wis Pagination work

--- src/js/functionSearch.js
+++ src/js/functionSearch.js
@@ -1,33 +1,31 @@
 import FetchData from './FetchData.js';
 import createCard from './filmCards-home.js';
+import pagination from './pagination.js';
 const cardsList = document.querySelector('.cards__list');
 const movieGalleryFetch = new FetchData();
 const form = document.querySelector('.search-form');
-const input = document.getElementById("inputSearch");
-const message = document.querySelector(".error-search");
+const input = document.getElementById('inputSearch');
+const message = document.querySelector('.error-search');
 
-form.addEventListener("submit", (e)=> {
-    e.preventDefault();
-    const word = input.value; // e.currentTarget.input выдает ошибку
-    // console.log(word) // слово для поиска 
-    movieGalleryFetch
+form.addEventListener('submit', e => {
+  e.preventDefault();
+  const word = input.value; // e.currentTarget.input выдает ошибку
+  // console.log(word) // слово для поиска
+  movieGalleryFetch
     .getSearchData(word)
     .then(response => {
-       console.log('даные которые пришли',response.data);
-       if(response.data.length == 0) {
-        message.classList.add("visible");
-        setTimeout(()=> message.classList.remove("visible"), 4000)
-       }
-       else {
-        cardsList.innerHTML = ""
-        createCard(response.data)
-       }
-        
+      console.log('даные которые пришли', response.data);
+      if (response.data.length == 0) {
+        message.classList.add('visible');
+        setTimeout(() => message.classList.remove('visible'), 4000);
+      } else {
+        //cardsList.innerHTML = '';
+          createCard(response.data);
+          pagination(response, word);
+      }
     })
     .catch(err => {
-        console.log('index err');
-        console.log(err);
+      console.log('index err');
+      console.log(err);
     });
-    
-})
-
+});
