6b11747650c257fc61f57a188096550a1269f9bf
Maxoverking <avemax7@gmail.com>
2022-12-07 11:10:49 +0100

    добавил библиотеку

--- src/js/filmCardsLibrary.js
+++ src/js/filmCardsLibrary.js
@@ -1,27 +1,25 @@
-import FetchData from './FetchData.js';
-import createCard from './filmCards-home.js';
-import { onClick } from './modal.js';
+import allGenres from './genres.json';
 
 const cardsListLibrary = document.querySelector('.cards__list--library');
-
 
 //Функция создания карточки на странице Home
 export default function createCardLibrary(data){
     const markup = data.map(obj => {
         const {id, poster_path, title, release_date, genre_ids} = obj;
         // console.log(obj);
-    return `<li class="cards__item" id="${id}">
+    return `<li class="cards__item" id="${id}" >
         <a class="cards__link">
             <img class="cards__img" src="https://image.tmdb.org/t/p/w500/${poster_path}" alt="${title}" loading="lazy">
         </a>
         <h2 class="cards__name">${title}</h2>
-        <p class="cards__genres"> ${genre_ids} | ${createYear(release_date)}</p>
+        <p class="cards__genres"> ${findGenresOfMovie(genre_ids)}  | ${createYear(release_date)}</p>
     </li>`
     }).join("");
 
     console.log(markup);
-    cardsListLibrary.innerHTML =  markup;
-    cardsListLibrary.addEventListener('click', onClick)
+    if (location.pathname === '/library.html') {
+cardsListLibrary.innerHTML =  markup;
+}
 }
 
 //Функция для отображения года выпуска
@@ -31,4 +29,20 @@ function createYear(data) {
     } else {
         return data = 'Not found';
     }
+}
+//Функция которая отвечает за жанр
+const { genres } = allGenres;
+export function findGenresOfMovie(ids) {
+    const arr = ids.flatMap(id => genres.filter(element => element.id === id));
+    let movieGenres = arr.map(el => el.name);
+    if (movieGenres.length > 2) {
+        const removedGenres = movieGenres.splice(0, 2);
+        removedGenres.push('Other');
+
+        return removedGenres.join(', ');
+    }
+    if (movieGenres.length === 0) {
+        return movieGenres = 'Not found';
+    }
+    return movieGenres.join(', ');
 }
